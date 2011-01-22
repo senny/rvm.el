9ab9b437719ea2a3d5581609b617089cb41e9915
Yves Senn <yves.senn@gmail.com>
2011-01-22 11:14:48 +0100

    tests are passing again

--- tests/rvm-integration-tests.el
+++ tests/rvm-integration-tests.el
@@ -37,111 +37,36 @@
 
 (require 'ert)
 
+(defun get-string-from-file (filePath)
+  "Return FILEPATH's file content."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+(defun get-rvm-stub (name)
+  (get-string-from-file (concat
+                         (file-name-directory (symbol-file 'get-rvm-stub))
+                         "rvm_stubs/"
+                         name)))
+
 (defun rvm--call-process (&rest args)
   (let ((command (first args))
         (arg1 (second args)))
-
     (cond
      ((and (string= command "list") (string= arg1 t))
-      "ruby-1.9.2-head")
+      (get-rvm-stub "rvm_list_default"))
      ((string= command "list")
-      "rvm Rubies
-
-   ruby-1.8.7-p249 [ x86_64 ]
-   ruby-1.9.1-p378 [ x86_64 ]
-=> ruby-1.9.2-head [ x86_64 ]
-   ruby-1.9.2-preview1 [ i386 ]
-   ruby-head [ x86_64 ]
-
-Default Ruby (for new shells)
-
-   ruby-1.9.2-head [ x86_64 ]
-
-System Ruby
-
-   system [ x86_64 i386 ppc ]")
+      (get-rvm-stub "rvm_list"))
      ((equal args '("ruby-1.9.2-head" "gemset" "list"))
-      "
-gemsets for ruby-1.9.2-head (found in /Users/senny/.rvm/gems/ruby-1.9.2-head)
-
-experimental
-global
-rails3
-rails3-beta4
-rails3beta")
+      (get-rvm-stub "ruby-1.9.2-head_rvm_gemset_list"))
      ((and (string= command "info") (string= arg1 "ruby-1.9.2-head"))
-      "
-system:
-  uname:        \"Darwin ymac.local 10.4.0 Darwin Kernel Version 10.4.0: Fri Apr 23 18:28:53 PDT 2010; root:xnu-1504.7.4~1/RELEASE_I386 i386\"
-  shell:        \"zsh\"
-  version:     \"4.3.9\"
-
-rvm:
-  type:         \"rvm is a shell function\"
-  version:      \"rvm 0.1.27 by Wayne E. Seguin (wayneeseguin@gmail.com) [http://rvm.beginrescueend.com/]\"
-
-ruby:
-  interpreter:  \"ruby\"
-  version:      \"1.9.2dev\"
-  date:         \"2010-03-22\"
-  platform:     \"x86_64-darwin10.2.0\"
-  patchlevel:   \"2010-03-22 trunk 27005\"
-  full_version: \"ruby 1.9.2dev (2010-03-22 trunk 27005) [x86_64-darwin10.2.0]\"
-
-homes:
-  gem:          \"/Users/senny/.rvm/gems/ruby-1.9.2-head\"
-  ruby:         \"/Users/senny/.rvm/rubies/ruby-1.9.2-head\"
-
-binaries:
-  ruby:         \"/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/ruby\"
-  irb:          \"/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/irb\"
-  gem:          \"/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/gem\"
-  rake:         \"/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/rake\"
-
-environment:
-  GEM_HOME:     \"/Users/senny/.rvm/gems/ruby-1.9.2-head\"
-  GEM_PATH:     \"/Users/senny/.rvm/gems/ruby-1.9.2-head:/Users/senny/.rvm/gems/ruby-1.9.2-head@global\"
-  BUNDLE_PATH:  \"/Users/senny/.rvm/gems/ruby-1.9.2-head\"
-  MY_RUBY_HOME: \"/Users/senny/.rvm/rubies/ruby-1.9.2-head\"
-  IRBRC:        \"/Users/senny/.rvm/rubies/ruby-1.9.2-head/.irbrc\"
-  gemset:       \"\"
-
-")
+      (get-rvm-stub "ruby-1.9.2-head_rvm_info"))
+     ((and (string= command "info") (string= arg1 "ruby-1.9.2-head@rails3"))
+      (get-rvm-stub "ruby-1.9.2-head@rails3_rvm_info"))
+     ((and (string= command "info") (string= arg1 "ruby-1.8.7-p249@experimental"))
+      (get-rvm-stub "ruby-1.8.7-p249@experimental_rvm_info"))
      ((and (string= command "info") (string= arg1 "ruby-1.8.7-p249"))
-      "system:
-  uname:        \"Darwin ymac.local 10.4.0 Darwin Kernel Version 10.4.0: Fri Apr 23 18:28:53 PDT 2010; root:xnu-1504.7.4~1/RELEASE_I386 i386\"
-  shell:        \"zsh\"
-  version:     \"4.3.9\"
-
-rvm:
-  type:         \"rvm is a shell function\"
-  version:      \"rvm 0.1.27 by Wayne E. Seguin (wayneeseguin@gmail.com) [http://rvm.beginrescueend.com/]\"
-
-ruby:
-  interpreter:  \"ruby\"
-  version:      \"1.8.7\"
-  date:         \"2010-01-10\"
-  platform:     \"i686-darwin10.2.0\"
-  patchlevel:   \"2010-01-10 patchlevel 249\"
-  full_version: \"ruby 1.8.7 (2010-01-10 patchlevel 249) [i686-darwin10.2.0]\"
-
-homes:
-  gem:          \"/Users/senny/.rvm/gems/ruby-1.8.7-p249\"
-  ruby:         \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249\"
-
-binaries:
-  ruby:         \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249/bin/ruby\"
-  irb:          \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249/bin/irb\"
-  gem:          \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249/bin/gem\"
-  rake:         \"/Users/senny/.rvm/gems/ruby-1.8.7-p249@global/bin/rake\"
-
-environment:
-  GEM_HOME:     \"/Users/senny/.rvm/gems/ruby-1.8.7-p249\"
-  GEM_PATH:     \"/Users/senny/.rvm/gems/ruby-1.8.7-p249:/Users/senny/.rvm/gems/ruby-1.8.7-p249@global\"
-  BUNDLE_PATH:  \"/Users/senny/.rvm/gems/ruby-1.8.7-p249\"
-  MY_RUBY_HOME: \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249\"
-  IRBRC:        \"/Users/senny/.rvm/rubies/ruby-1.8.7-p249/.irbrc\"
-  gemset:       \"\""))))
+      (get-rvm-stub "ruby-1.8.7-p249_rvm_info")))))
 
 (defun should-be-rvm-environment (ruby-binaries gemhome gempath)
   (should (equal (rvm--emacs-ruby-binary) ruby-binaries))
@@ -154,11 +79,11 @@ environment:
   (rvm-use-default)
   (should-be-rvm-environment
    '("/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/")
-   "/Users/senny/.rvm/gems/ruby-1.9.2-head@global"
+   "/Users/senny/.rvm/gems/ruby-1.9.2-head"
    "/Users/senny/.rvm/gems/ruby-1.9.2-head:/Users/senny/.rvm/gems/ruby-1.9.2-head@global")
   (funcall body))
 
-(deftest rvm-test-info ()
+(deftest rvm-test-rvm/info ()
   (let* ((result (rvm/info "ruby-1.8.7-p249"))
          (version (cdr (assoc "version" result)))
          (gem_home (cdr (assoc "GEM_HOME" result)))
@@ -171,17 +96,14 @@ environment:
     (should (string= gemset nil))
     (should (string= binary "/Users/senny/.rvm/rubies/ruby-1.8.7-p249/bin/ruby"))))
 
-(deftest rvm-test-list ()
+(deftest rvm-test-rvm/list ()
   (let* ((result (rvm/list)))
     (should (equal result
-                   '("ruby-1.9.2-head" "ruby-1.8.7-p249" "ruby-1.9.1-p378" "ruby-1.9.2-preview1" "ruby-head" "system")))))
+                   '("ruby-1.9.2-head" "ruby-1.8.7-p249" "ruby-1.9.1-p378" "ruby-1.9.2-preview1" "ruby-head")))))
 
-(deftest rvm-test-gemset-list ()
+(deftest rvm-test-rvm/gemset-list ()
   (let* ((result (rvm/gemset-list "ruby-1.9.2-head")))
     (should (equal result '("experimental" "global" "rails3" "rails3-beta4" "rails3beta")))))
-
-(deftest rvm-test-rvmrc-read-version ()
-  (should (equal (rvm--rvmrc-read-version ".rvmrc") '("ruby-1.9.2-head" "rails3"))))
 
 (deftest rvm-test-rvm-use ()
   (rvm-test-environment (lambda ()
@@ -189,7 +111,7 @@ environment:
                           (should-be-rvm-environment
                            '("/Users/senny/.rvm/rubies/ruby-1.8.7-p249/bin/")
                            "/Users/senny/.rvm/gems/ruby-1.8.7-p249@experimental"
-                           "/Users/senny/.rvm/gems/ruby-1.8.7-p249:/Users/senny/.rvm/gems/ruby-1.8.7-p249@experimental")
+                           "/Users/senny/.rvm/gems/ruby-1.8.7-p249@experimental:/Users/senny/.rvm/gems/ruby-1.8.7-p249@global")
                           )))
 
 (deftest rvm-test-activate-corresponding-ruby ()
@@ -198,7 +120,7 @@ environment:
                           (should-be-rvm-environment
                            '("/Users/senny/.rvm/rubies/ruby-1.9.2-head/bin/")
                            "/Users/senny/.rvm/gems/ruby-1.9.2-head@rails3"
-                           "/Users/senny/.rvm/gems/ruby-1.9.2-head:/Users/senny/.rvm/gems/ruby-1.9.2-head@rails3")
+                           "/Users/senny/.rvm/gems/ruby-1.9.2-head@rails3:/Users/senny/.rvm/gems/ruby-1.9.2-head@global")
                           )))
 
 (ert-run-tests-interactively "rvm-.*")
